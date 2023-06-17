0bed1000e0d27cb7dbaa68dc3cef03aafc2bd49a
rzzkv <rzzkv@yandex.ru>
2023-06-17 06:34:43 +0500

    s

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        double result = toBeDivided / 5.0;
+        System.out.println("Result: " + result);
     }
 }
